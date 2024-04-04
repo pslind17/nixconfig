f0e336d55b1a1e1de213d1335268f0d4f69f6f2e
p1diddy <125328571+p1diddy@users.noreply.github.com>
2024-04-04 16:03:21 -0700

    still setting up

---
+++ nixosModules/xfce.nix
@@ -0,0 +1,17 @@
+# xfce.nix
+
+{ pkgs, lib, config, ... }: {
+
+  services.xserver.enable = true;
+
+  # Enable the XFCE Desktop Environment.
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.desktopManager.xfce.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+  
+  }
