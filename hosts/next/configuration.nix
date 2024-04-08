acca46dc35f167493b5ac50050c1f3b3d29f0f4b
p1diddy <125328571+p1diddy@users.noreply.github.com>
2024-04-08 16:46:32 -0700

    mistake

--- hosts/next/configuration.nix
+++ hosts/next/configuration.nix
@@ -13,9 +13,9 @@
     ];
 
   # Bootloader.
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
-
+    boot.loader.grub.enable = true;
+    boot.loader.grub.device = "/dev/sda";
+    boot.loader.grub.useOSProber = true;
 
   networking.hostName = "next"; # Define your hostname.
 
