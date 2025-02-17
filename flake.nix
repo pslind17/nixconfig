1f7eece4fa59a7656871941e5a639e80df3a72a0
p1diddy <125328571+p1diddy@users.noreply.github.com>
2025-02-17 13:17:30 -0800

    update to 24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS desktop ocnfiguration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
   };
 
   outputs = { self, nixpkgs }:
