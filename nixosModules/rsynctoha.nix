95dc200cfb27b641c2a70f5af3d5e931a46dfacf
pslind17 <peytonjslind@gmail.com>
2025-09-01 12:41:07 -0700

    rsync

---
+++ nixosModules/rsynctoha.nix
@@ -0,0 +1,20 @@
+# ...existing code...
+    environment.systemPackages = with pkgs; [
+      rsync
+      cifs-utils   # <--- Add this line
+    ];
+# ...existing code...
+# ...existing code...
+  fileSystems."/mnt/sambashare" = {
+    device = "homeassistant.local/media";
+    fsType = "cifs";
+    options = [
+      "username=pslind"
+      "password=EE95Lv!H"
+      "uid=1000"  # Replace with your user ID
+      "gid=100"   # Replace with your group ID
+      "rw"
+      "vers=3.0"  # Or the version your server supports
+    ];
+  };
+# ...existing code...
