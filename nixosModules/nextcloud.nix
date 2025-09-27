3c1cb3e6c3dfc486422a4108ad30440049990b88
pslind17 <peytonjslind@gmail.com>
2025-09-26 21:51:19 -0700

    refactor Nextcloud configuration for improved indentation and structure

--- nixosModules/nextcloud.nix
+++ nixosModules/nextcloud.nix
@@ -1,12 +1,12 @@
-{ self, config, lib, pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
   users.users.pslind.createHome = true;
   users.users.pslind.homeMode = "750";
 
-    networking.firewall.allowedTCPPorts = [ 80 443 ];
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
 
-    services.nginx.virtualHosts."next" = {
+  services.nginx.virtualHosts."next" = {
     root = "${pkgs.nextcloud31}/share/nextcloud";
     locations."/" = {
       proxyPass = "http://127.0.0.1:8080/";
@@ -14,32 +14,24 @@
     # Add SSL config if using https
   };
 
-    nextcloud = {
-      enable = true;
-      hostName = "next";
+  services.nextcloud = {
+    enable = true;
+    hostName = "next";
+    package = pkgs.nextcloud31;
+    database.createLocally = true;
+    configureRedis = true;
+    maxUploadSize = "16G";
+    https = true;
 
-       # Need to manually increment with every major upgrade.
-      package = pkgs.nextcloud31;
-
-      # Let NixOS install and configure the database automatically.
-      database.createLocally = true;
-
-      # Let NixOS install and configure Redis caching automatically.
-      configureRedis = true;
-
-      # Increase the maximum file upload size to avoid problems uploading videos.
-      maxUploadSize = "16G";
-      https = true;
-
-      settings = {
-        overwriteProtocol = "http";
-        defaultPhoneRegion = "PT";
-      };
-
-      config = {
-        dbtype = "pgsql";
-        adminuser = "pslind";
-        adminpassFile = "/home/pslind/nextcloud.pass";
-      };
+    settings = {
+      overwriteProtocol = "http";
+      defaultPhoneRegion = "PT";
     };
-  }
+
+    config = {
+      dbtype = "pgsql";
+      adminuser = "pslind";
+      adminpassFile = "/home/pslind/nextcloud.pass";
+    };
+  };
+}
