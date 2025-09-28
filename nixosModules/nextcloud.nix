ba40e5cfd0e5acd17f44a5f4a676f60a7e3ddbee
pslind17 <peytonjslind@gmail.com>
2025-09-28 15:42:37 -0700

    thsh

--- nixosModules/nextcloud.nix
+++ nixosModules/nextcloud.nix
@@ -4,14 +4,9 @@
   services.nextcloud = {
     enable = true;
     package = pkgs.nextcloud31;
-
     datadir = "/var/lib/nextcloud";
     hostName = "localhost";
-
-    # Explicit DB type required in >=25.05
     config.dbtype = "sqlite";
-
-    # Disable auto-setup so no adminpassFile is required
     autoSetup = false;
   };
 
