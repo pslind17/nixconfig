27b44a326f18059a35bd1750bcaa1126b77b95e6
pslind17 <peytonjslind@gmail.com>
2025-09-28 15:34:36 -0700

    ge

--- nixosModules/nextcloud.nix
+++ nixosModules/nextcloud.nix
@@ -1,28 +1,35 @@
 { config, pkgs, ... }:
 
 {
+  # For setting the Nextcloud admin password file (if desired)
+  environment.etc."nextcloud-admin-pass".text = "youradminpassword";
+
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud29;
+    package = pkgs.nextcloud31;
 
     datadir = "/var/lib/nextcloud";
-    hostName = "cloud.example.com";
+    hostName = "localhost";
 
-    # Database (must be explicit)
-    database = {
-      createLocally = true;
-      type = "sqlite";  # alternatives: "mysql" or "sqlite"
-    };
+    # Use SQLite for testing / minimal usage (not for production)
+    config.dbtype = "sqlite";
 
-    # Redis cache
-    configureRedis = true;
-
-    # HTTPS handled by nginx
-    https = false;
+    # Optionally, set the admin password via the file we made above
+    config.adminpassFile = "/etc/nextcloud-admin-pass";
   };
 
   services.nginx = {
     enable = true;
+    virtualHosts."localhost" = {
+      # Listen on port 8080 (HTTP) by default
+      listen = [ { addr = "0.0.0.0"; port = 8080; } ];
+
+      root = "${config.services.nextcloud.package}/share/nextcloud";
+
+      locations."/" = {
+        proxyPass = "http://unix:/run/nextcloud-phpfpm/nextcloud.sock";
+      };
+    };
   };
 
   services.phpfpm.pools.nextcloud = {
@@ -33,14 +40,8 @@
       "listen" = "/run/nextcloud-phpfpm/nextcloud.sock";
       "listen.owner" = "nginx";
       "listen.group" = "nginx";
-      "pm" = "dynamic";
-      "pm.max_children" = "32";
-      "pm.start_servers" = "2";
-      "pm.min_spare_servers" = "2";
-      "pm.max_spare_servers" = "4";
     };
   };
 
-  networking.firewall.allowedTCPPorts = [ 80 443 ];
-
+  networking.firewall.allowedTCPPorts = [ 8080 ];
 }
