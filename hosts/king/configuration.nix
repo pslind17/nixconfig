b42d75190de791b99e689696f794b4d5124ae982
pslind17 <peytonjslind@gmail.com>
2025-09-30 14:43:07 -0700

    cosmic

--- hosts/king/configuration.nix
+++ hosts/king/configuration.nix
@@ -9,7 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ../../nixosModules/basics.nix
-      ../../nixosModules/plasma.nix
+      ../../nixosModules/cosmic.nix
       ../../nixosModules/desktopapps.nix
       ../../nixosModules/rsynctoha.nix
     ];
---
+++ nixosModules/cosmic.nix
@@ -0,0 +1,5 @@
+{ self, config, lib, pkgs, ... }:
+
+{
+  services.desktopManager.cosmic.enable = true;
+}
