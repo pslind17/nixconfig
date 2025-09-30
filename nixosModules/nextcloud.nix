dd1ed8583ae215039cdc0365419ecbbef8e019ea
pslind17 <peytonjslind@gmail.com>
2025-09-30 12:39:08 -0700

    dhmn

--- nixosModules/nextcloud.nix
+++ nixosModules/nextcloud.nix
@@ -3,32 +3,14 @@
 {
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud29; # or omit for default
+    package = pkgs.nextcloud29; # optional
     hostName = "localhost";
     https = false;
 
-    config = {
-      dbtype = "pgsql";
-      dbuser = "nextcloud";
-      dbpass = "super-strong-db-password"; # must be plain string
-      dbname = "nextcloud";
-    };
+    config.dbtype = "sqlite";
 
     nginx.listen = [
       { addr = "0.0.0.0"; port = 8123; ssl = false; }
-    ];
-  };
-
-  services.postgresql = {
-    enable = true;
-    ensureDatabases = [ "nextcloud" ];
-    ensureUsers = [
-      {
-        name = "nextcloud";
-        ensureDBOwnership = true;
-        # This sets the same password as above
-        password = "super-strong-db-password";
-      }
     ];
   };
 
