7670724237330f933eaabfdbb7b2c5808a61d3c3
pslind17 <peytonjslind@gmail.com>
2025-09-30 13:19:34 -0700

    raeg

--- nixosModules/nextcloud.nix
+++ nixosModules/nextcloud.nix
@@ -1,36 +1,13 @@
 { self, config, lib, pkgs, ... }:
 
 {
-  services = {
-
-
-    nextcloud = {
-      enable = true;
-      hostName = "next";
-
-       # Need to manually increment with every major upgrade.
-      package = pkgs.nextcloud31;
-
-      # Let NixOS install and configure the database automatically.
-      database.createLocally = true;
-
-      # Let NixOS install and configure Redis caching automatically.
-      configureRedis = true;
-
-      # Increase the maximum file upload size to avoid problems uploading videos.
-      maxUploadSize = "16G";
-      https = true;
-
-      config = {
-        overwriteProtocol = "https";
-        defaultPhoneRegion = "PT";
-        dbtype = "pgsql";
-        adminuser = "admin";
-        adminpassFile = "/var/lib/nextcloud/admin-pass";
-      };
-    };
-  };
-
-  networking.firewall.allowedTCPPorts = [ 80 443 ];
+  environment.etc."nextcloud-admin-pass".text = "plsworkijustwannabackupmydesktop";
+services.nextcloud = {
+  enable = true;
+  package = pkgs.nextcloud31;
+  hostName = "localhost";
+  config.adminpassFile = "/etc/nextcloud-admin-pass";
+  config.dbtype = "sqlite";
+};
 }
 
