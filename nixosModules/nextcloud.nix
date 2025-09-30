7bbbc35d8fda9dc1cb030c87b8778f4edc21a9ca
pslind17 <peytonjslind@gmail.com>
2025-09-30 12:39:53 -0700

    htresh

--- nixosModules/nextcloud.nix
+++ nixosModules/nextcloud.nix
@@ -3,15 +3,24 @@
 {
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud29; # optional
+    package = pkgs.nextcloud29; # or omit
     hostName = "localhost";
     https = false;
 
-    config.dbtype = "sqlite";
+    config.dbtype = "sqlite"; # simplest (no PostgreSQL service needed)
+  };
 
-    nginx.listen = [
-      { addr = "0.0.0.0"; port = 8123; ssl = false; }
-    ];
+  services.nginx = {
+    enable = true;
+
+    virtualHosts."localhost" = {
+      forceSSL = false;
+      listen = [{
+        addr = "0.0.0.0";
+        port = 8123;
+        ssl = false;
+      }];
+    };
   };
 
   networking.firewall.allowedTCPPorts = [ 8123 ];
