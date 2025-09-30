47a4908e9af1419d50aa51654fd0a3df6d9a57c6
pslind17 <peytonjslind@gmail.com>
2025-09-30 09:23:17 -0700

    dv

--- hosts/next/configuration.nix
+++ hosts/next/configuration.nix
@@ -9,7 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ../../nixosModules/basics.nix
-      ../../nixosModules/nextcloud.nix
+      ../../nixosModules/vscodeserver.nix
     ];
 
   # Bootloader.
