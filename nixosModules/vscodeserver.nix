58eb6f0fdb44bd770634aeae32e3c1e4b8c54e1c
pslind17 <peytonjslind@gmail.com>
2025-09-30 09:24:44 -0700

    zg

--- nixosModules/vscodeserver.nix
+++ nixosModules/vscodeserver.nix
@@ -2,7 +2,7 @@
 
 {
   services.openvscode-server.enable = true;
-  services.openvscode-server.port = 8;
+  services.openvscode-server.port = 80;
 
   networking.firewall.allowedTCPPorts = [ 80 ];
 }
