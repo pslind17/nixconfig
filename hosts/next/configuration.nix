18d5b53149dfe97689beca03da115c21c286cc87
pslind17 <peytonjslind@gmail.com>
2025-11-05 15:35:57 -0800

    Remove vscodeserver module from configuration

--- hosts/next/configuration.nix
+++ hosts/next/configuration.nix
@@ -9,7 +9,6 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ../../nixosModules/basics.nix
-      ../../nixosModules/vscodeserver.nix
     ];
 
   # Bootloader.
