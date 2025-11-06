b99930468fd61518ac48a9a7de0cc658c45122fb
pslind17 <peytonjslind@gmail.com>
2025-11-05 22:04:45 -0800

    Refactor Vaultwarden configuration to streamline allowed TCP ports setup

--- nixosModules/vaultwarden.nix
+++ nixosModules/vaultwarden.nix
@@ -4,13 +4,7 @@
   # ...existing imports/config...
 
   services.vaultwarden.enable = true;
+  networking.firewall.allowedTCPPorts = [ 8000 443 ];
 
-  systemd.services.vaultwarden.serviceConfig = {
-    Environment = ''
-      ROCKET_ADDRESS=0.0.0.0
-      ROCKET_PORT=8000
-    '';
-  };
 
-  networking.firewall.allowedTCPPorts = lib.mkForce ((config.networking.firewall.allowedTCPPorts or []) ++ [ 8000 ]);
 }
