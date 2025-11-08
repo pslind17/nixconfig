bb962f380438877f51fa51df6677cba12937e5e5
pslind17 <peytonjslind@gmail.com>
2025-11-08 11:16:57 -0800

    Refactor Nextcloud configuration to streamline setup and adjust database settings

--- nixosModules/nextcloud.nix
+++ nixosModules/nextcloud.nix
@@ -1,27 +1,10 @@
 { self, config, lib, pkgs, ... }: {
-  services = {
-    nginx = {
-      enable = true;            # ensure nginx is enabled
-    };
-
-    nextcloud = {
-      enable = true;
-      hostName = "next";
-      package = pkgs.nextcloud31;
-      database.createLocally = true;
-      configureRedis = true;
-      maxUploadSize = "16G";
-      https = false;            # set false for testing, enable when certs are configured
-      settings = {
-        overwriteProtocol = "http";
-        default_phone_region = "US";
-      };
-      config = {
-        dbtype = "pgsql";
-        adminuser = "admin";
-        adminpassFile = "/home/pslind/adminpass";
-      };
-      phpOptions."opcache.interned_strings_buffer" = "16";
-    };
-  };
+  environment.etc."nextcloud-admin-pass".text = "FRERGge452greha!$";
+services.nextcloud = {
+  enable = true;
+  package = pkgs.nextcloud31;
+  hostName = "localhost";
+  config.adminpassFile = "/etc/nextcloud-admin-pass";
+  config.dbtype = "sqlite";
+};
 }
