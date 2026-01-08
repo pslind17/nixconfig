2952c8ab9793fa25beba0ec52449439b79156c52
pslind17 <peytonjslind@gmail.com>
2026-01-07 16:38:09 -0800

    a

--- nixosModules/nextcloud.nix
+++ nixosModules/nextcloud.nix
@@ -3,7 +3,8 @@
   services.nextcloud = {
     package = pkgs.nextcloud32;
     enable = true;
-    hostName = "next";
+    https = true;
+    hostName = "next.tail26b773.ts.net";
     database.createLocally = true;
     config = {
       dbtype = "pgsql";
