53ca39bca383da197e100018f25173697744c5f3
pslind17 <peytonjslind@gmail.com>
2026-01-07 19:01:50 -0800

    a

--- nixosModules/nextcloud.nix
+++ nixosModules/nextcloud.nix
@@ -1,41 +1,18 @@
 { pkgs, ... }:
-
 {
-  ############################
-  # Nextcloud (manages nginx)
-  ############################
   services.nextcloud = {
-    enable = true;
     package = pkgs.nextcloud32;
-
-    hostName = "next";
-    https = true;
-
+    enable = true;
+    hostName = "next.tail26b773.ts.net";
     database.createLocally = true;
-
     config = {
       dbtype = "pgsql";
       adminpassFile = "/home/pslind/nixconfig/nixosModules/admin-pass-file";
     };
   };
 
-  ############################
-  # Nginx (MERGE SSL ONLY)
-  ############################
-  services.nginx = {
-    enable = true;
-
-    virtualHosts."next" = {
-      forceSSL = true;
-      enableACME = false;
-
-      sslCertificate = "/var/lib/nginx/next.crt";
-      sslCertificateKey = "/var/lib/nginx/next.key";
-    };
-  };
-
-  ############################
-  # Firewall
-  ############################
-  networking.firewall.allowedTCPPorts = [ 443 ];
+  networking.firewall.allowedTCPPorts = [
+    80
+    443
+  ];
 }
