a4e8a6a0b392409afcfa996e37b2beaad2ec9684
pslind17 <peytonjslind@gmail.com>
2026-01-11 10:19:53 -0800

    w

--- nixosModules/vaultwarden.nix
+++ nixosModules/vaultwarden.nix
@@ -1,16 +1,21 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, ... }:
 
 {
   services.vaultwarden = {
-  enable = true;
-  dbBackend = "postgresql";
-  # Store your variables like admin password here
-  environmentFile = "/home/pslind/vaultwarden/.env";
-  config = {
-    SIGNUPS_ALLOWED = false;
-    DOMAIN = "http://next";
-  };
-};
-  networking.firewall.allowedTCPPorts = [ 8000 ];
+    enable = true;
 
+    config = {
+      # IMPORTANT: must be https
+      DOMAIN = "https://vaultwarden.${config.networking.hostName}.ts.net";
+
+      ROCKET_ADDRESS = "127.0.0.1";
+      ROCKET_PORT = 8222;
+
+      WEBSOCKET_ENABLED = true;
+      SIGNUPS_ALLOWED = false;
+    };
+  };
+
+  # Vaultwarden does NOT need firewall access
+  # because only Tailscale talks to it
 }
