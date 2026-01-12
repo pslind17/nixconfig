d9be3e805b82de74d30572a4c1085bc4ea2de461
pslind17 <peytonjslind@gmail.com>
2026-01-12 15:33:59 -0800

    a

--- nixosModules/nextcloud.nix
+++ nixosModules/nextcloud.nix
@@ -1,22 +1,25 @@
 { pkgs, config, ... }:
 {
   services.nextcloud = {
-    package = pkgs.nextcloud32;
     enable = true;
+    package = pkgs.nextcloud32;
     hostName = "next.tail26b773.ts.net";
+
     database.createLocally = true;
+
     config = {
       dbtype = "pgsql";
       adminpassFile = "/home/pslind/nixconfig/nixosModules/admin-pass-file";
     };
 
-    extraApps = {
-    inherit (config.services.nextcloud.package.packages.apps) news contacts calendar tasks notes;
     extraAppsEnable = true;
 
+    extraApps = {
+      inherit (config.services.nextcloud.package.packages.apps)
+        news contacts calendar tasks notes;
+    };
   };
-  };  
-  
+
   networking.firewall.allowedTCPPorts = [
     80
     443
