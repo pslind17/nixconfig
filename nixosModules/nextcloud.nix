430d85e75b33a917b2d88bf19d827ac26fa2a36e
pslind17 <peytonjslind@gmail.com>
2026-01-12 15:27:57 -0800

    6

--- nixosModules/nextcloud.nix
+++ nixosModules/nextcloud.nix
@@ -10,9 +10,15 @@
       adminpassFile = "/home/pslind/nixconfig/nixosModules/admin-pass-file";
     };
 
-  extraApps = {
-    inherit (config.services.nextcloud.package.packages.apps) calendar tasks notes;
-  extraAppsEnable = true;
+    extraApps = {
+    inherit (config.services.nextcloud.package.packages.apps) news contacts calendar tasks;
+    notes = pkgs.fetchNextcloudApp {
+        sha256 = "sha256-Xr1SRSmXo2r8yOGuoMyoXhD0oPVm/0/ISHlmNZpJYsg=";
+        url = "https://github.com/nextcloud-releases/notes/releases/download/v4.12.4/notes-v4.12.4.tar.gz";
+        license = "agpl3";
+    };
+    extraAppsEnable = true;
+
   };
 
   networking.firewall.allowedTCPPorts = [
