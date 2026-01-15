3ccd5a1caf59c69994f4972738014eb0990df149
pslind17 <peytonjslind@gmail.com>
2026-01-14 20:38:54 -0800

    g

--- nixosModules/zfs.nix
+++ nixosModules/zfs.nix
@@ -3,4 +3,14 @@
   services.zfs.autoScrub.enable = true;
 
   networking.hostId = "54a31bce"; # 8 hex chars
+
+  services.zfs.autoSnapshot = {
+  enable = true;
+  frequent = 4;
+  hourly = 24;
+  daily = 7;
+  weekly = 4;
+  monthly = 3;
+};
+
 }
