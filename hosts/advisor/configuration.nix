e45b3d82396fadb148013ff894b13d48d639a6ee
pslind17 <peytonjslind@gmail.com>
2026-01-15 17:49:05 -0800

    a

--- hosts/advisor/configuration.nix
+++ hosts/advisor/configuration.nix
@@ -13,11 +13,6 @@
     ];
 
   # Bootloader.
-  #boot.loader.systemd-boot.enable = true;
-  #boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
