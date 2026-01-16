91bc0f1e988dcd00e669413fc117885749682d1c
pslind17 <peytonjslind@gmail.com>
2026-01-15 17:45:55 -0800

    w

--- hosts/advisor/configuration.nix
+++ hosts/advisor/configuration.nix
@@ -18,6 +18,9 @@
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
 
   networking.hostName = "advisor"; # Define your hostname.
 
