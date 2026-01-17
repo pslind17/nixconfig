9f827bf9025ecb3864344857ccb9b1debe6ad8b7
pslind17 <peytonjslind@gmail.com>
2026-01-17 14:13:32 -0800

    s

--- nixosModules/kuma.nix
+++ nixosModules/kuma.nix
@@ -1,6 +1,13 @@
-{ pkgs, config, ... }: {
-    services.uptime-kuma.enable = true;
-    networking.firewall.allowedTCPPorts = [
-    3001
-  ];
+{ pkgs, config, ... }:
+
+{
+  services.uptime-kuma = {
+    enable = true;
+    settings = {
+      HOST = "squire";
+      PORT = "3001";
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [ 3001 ];
 }
