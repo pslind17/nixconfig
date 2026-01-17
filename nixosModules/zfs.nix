de98595c858d9703445ac2c618ea2470e1229835
pslind17 <peytonjslind@gmail.com>
2026-01-16 17:00:01 -0800

    s

--- nixosModules/zfs.nix
+++ nixosModules/zfs.nix
@@ -6,4 +6,14 @@ boot.supportedFilesystems = [ "zfs" ];
 services.zfs = {
   autoScrub.enable = true;
 };
+
+services.zfs.autoSnapshot = {
+  enable = true;
+  frequent = 4;
+  hourly = 24;
+  daily = 7;
+  weekly = 4;
+  monthly = 3;
+};
+
 }
