8115f24d42535f6bb5d26ffca7c775d64d202a9f
pslind17 <peytonjslind@gmail.com>
2026-01-16 16:40:08 -0800

    y

--- nixosModules/zfs.nix
+++ nixosModules/zfs.nix
@@ -1,30 +1,7 @@
 { pkgs, lib, config, ... }: {
-  boot.supportedFilesystems = [ "zfs" ];
-  services.zfs.autoScrub.enable = true;
+boot.supportedFilesystems = [ "zfs" ];
 
-  networking.hostId = "54a31bce"; # 8 hex chars
-
-  services.zfs.autoSnapshot = {
-  enable = true;
-  frequent = 4;
-  hourly = 24;
-  daily = 7;
-  weekly = 4;
-  monthly = 3;
+services.zfs = {
+  autoScrub.enable = true;
 };
-
-environment.systemPackages = [
-  pkgs.borgbackup
-];
-
-users.users.backup = {
-  isSystemUser = true;
-  home = "/home/backup";
-  createHome = true;
-  shell = pkgs.bash;  # or pkgs.shadow.nologin (see below)
-  group = "users";
-};
-
-
-
 }
