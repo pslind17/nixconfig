178c48ab77d234fe6fafb8adf2e2b20e1ff3709f
pslind17 <peytonjslind@gmail.com>
2026-01-18 10:15:58 -0800

    g

--- nixosModules/guacamole.nix
+++ nixosModules/guacamole.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }: {
        services.guacamole-server = {
        enable = true;
-       host = "127.0.0.1";
+       host = "0.0.0.0";
        port = 4822;
        userMappingXml = ./user-mapping.xml;
    };
