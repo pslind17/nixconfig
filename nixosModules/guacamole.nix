9b1019ecfc23cfa225d4f052021afeb0405b3836
pslind17 <peytonjslind@gmail.com>
2026-01-18 10:22:37 -0800

    f

--- nixosModules/guacamole.nix
+++ nixosModules/guacamole.nix
@@ -1,9 +1,30 @@
-{ config, pkgs, ... }: {
-       services.guacamole-server = {
-       enable = true;
-       host = "127.0.0.1";
-       port = 4822;
-       userMappingXml = ./user-mapping.xml;
-   };
-   networking.firewall.allowedTCPPorts = [ 4822 ];
+{ config, pkgs, ... }:
+
+{
+  services.guacamole-server.enable = true;
+
+  services.guacamole-client = {
+    enable = true;
+    userMappingXml = ./user-mapping.xml;
+  };
+
+  services.nginx = {
+    enable = true;
+
+    virtualHosts."guacamole" = {
+      listen = [
+        {
+          addr = "0.0.0.0";
+          port = 8085;
+        }
+      ];
+
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:8080";
+        proxyWebsockets = true;
+      };
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [ 8085 ];
 }
