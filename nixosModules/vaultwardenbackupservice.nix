e8f5fea91cda7581ddba80797d76282142ba611e
pslind17 <peytonjslind@gmail.com>
2026-01-20 15:17:39 -0800

    s

--- nixosModules/vaultwardenbackupservice.nix
+++ nixosModules/vaultwardenbackupservice.nix
@@ -1,25 +1,23 @@
 { pkgs, lib, config, ... }: {
-  services.borgbackup.jobs.vaultwarden = {
-    paths = [ "/var/lib/bitwarden_rs" ];
-    repo = "borg@advisor:/srv/vaultwarden/borg";
+services.borgbackup.jobs.vaultwarden = {
+  paths = [ "/var/lib/bitwarden_rs" ];
+  repo = "borg@advisor:/srv/vaultwarden/borg";
 
-    compression = "zstd";
-    startAt = "daily";
-
-    encryption = {
-      mode = "repokey-blake2";
-      passCommand = "cat /run/secrets/borg-vaultwarden-pass";
-    };
-
-    prune.keep = {
-      daily = 7;
-      weekly = 4;
-      monthly = 3;
-    };
-
-    # 👇 ADD THIS
-    environment = {
-      BORG_RSH = "ssh -i /home/pslind/.ssh/borg_vaultwarden -o IdentitiesOnly=yes";
-    };
+  encryption = {
+    mode = "repokey-blake2";
+    passCommand = "cat /run/secrets/borg-vaultwarden-pass";
   };
+
+  compression = "zstd";
+  startAt = "daily";
+
+  prune.keep = {
+    daily = 7;
+    weekly = 4;
+    monthly = 3;
+  };
+
+  extraArgs = [ "--rsh=ssh -i /var/lib/borg/id_ed25519 -o IdentitiesOnly=yes" ];
+};
+
 }
