f7b690ceab224eed35e1dea207d6a06182b7424c
pslind17 <peytonjslind@gmail.com>
2026-01-20 15:24:58 -0800

    s

--- nixosModules/basics.nix
+++ nixosModules/basics.nix
@@ -13,6 +13,7 @@
     openssl
     lm_sensors # Configure with "sudo sensors-detect" and use "sensors" to view
     borgbackup
+    atuin
     ];
 
     boot.kernelModules = [ "xhci_pci" "usb_storage" "uas" ];
