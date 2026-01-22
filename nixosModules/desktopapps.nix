2dac9218b292868480adcc232ddd9687c8346947
pslind17 <peytonjslind@gmail.com>
2026-01-22 15:15:27 -0800

    q

--- nixosModules/desktopapps.nix
+++ nixosModules/desktopapps.nix
@@ -29,6 +29,13 @@
     bitwarden-cli
   ];
 
+  programs.steam = {
+  enable = true;
+  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+};
+
     nixpkgs.config.permittedInsecurePackages = [
     "electron-19.1.9"
   ];
