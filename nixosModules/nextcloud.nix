e90c29169f778d8d39edd176ff92571a5395b157
pslind17 <peytonjslind@gmail.com>
2026-02-11 16:51:14 -0800

    fix: remove assistant app from extraApps in Nextcloud configuration

--- nixosModules/nextcloud.nix
+++ nixosModules/nextcloud.nix
@@ -16,14 +16,7 @@
 
     extraApps = {
       inherit (config.services.nextcloud.package.packages.apps)
-        news contacts calendar tasks notes integration_openai;
-
-        assistant = pkgs.fetchNextcloudApp rec {
-  name = "assistant";
-  url = "https://github.com/nextcloud-releases/assistant/releases/download/v3.1.0/assistant-v3.1.0.tar.gz";
-  sha256 = "sha256-ANs6OygrtlZAQZ9TWTimu3Bo8RCeYf5BkcW7YZWsMEY=";
-  license = "agpl3Plus";
-};
+        news contacts calendar tasks notes integration_openai textprocessing;
 
     };
   };
