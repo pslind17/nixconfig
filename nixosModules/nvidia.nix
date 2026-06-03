bab19d4ecc0aa8875997319a45c254aa2f1de03b
pslind17 <peytonjslind@gmail.com>
2026-06-02 17:41:20 -0700

    wf

--- nixosModules/nvidia.nix
+++ nixosModules/nvidia.nix
@@ -2,46 +2,9 @@
 
 {
   nixpkgs.config.allowUnfree = true;
-
-  # 1. Base Graphics & Wayland support
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
-
+  hardware.graphics.enable = true;
   services.xserver.videoDrivers = [ "nvidia" ];
-
-  # 2. Nvidia Driver Configuration for Pascal + Wayland
-  hardware.nvidia = {
-    # Modesetting is STRICTLY required for Wayland
-    modesetting.enable = true;
-
-    # Fixes graphical corruption/flicker after suspend
-    powerManagement.enable = true;
-    powerManagement.finegrained = false;
-
-    # MUST be false for GTX 1080 (open-source modules break Pascal Wayland)
-    open = false; 
-
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
-
-  # 3. Environment variables for Wayland + Nvidia
-  environment.sessionVariables = {
-    # Hint Electron apps (Discord, VS Code) to use Wayland natively
-    NIXOS_OZONE_WL = "1";
-    
-    # Force hardware acceleration in Clutter-based applications
-    CLUTTER_BACKEND = "wayland";
-    
-    # Directs Qt applications to utilize Wayland
-    QT_QPA_PLATFORM = "wayland";
-    
-    # Required if using a wlroots compositor (like Hyprland or Sway)
-    WLR_NO_HARDWARE_CURSORS = "1";
-    KWIN_FORCE_SW_CURSORS = "1";
-  };
+  hardware.nvidia.open = false;  # see the note above
 
   
 }
